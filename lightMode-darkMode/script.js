8d96e769fd202c646b30c76d9c1c045f6c5d1241
Harithus Avgs <hariavgs@gmail.com>
2024-03-24 10:24:19 +0530

    Add files via upload

---
+++ lightMode-darkMode/script.js
@@ -0,0 +1,16 @@
+let body = document.querySelector("body");
+let btn1 = document.querySelector("#btn1");
+let mode = "light";
+
+btn1.addEventListener("click", () => {
+    if(mode === "light"){
+        mode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }
+    else{
+        mode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+});
